5881ec63736de157876dedd77ded42369bc0e7ee
JO JUNGYUN <66199062+JUNGYUN-Daegu@users.noreply.github.com>
2020-10-05 16:06:24 +0900

    Print Statement

---
+++ HelloWorld.py
@@ -0,0 +1,24 @@
+#Full quotation mark and semi-quotation mark are both available to print Str
+print("Hello World")
+print('풍선')
+
+#Compound of Str and Int is possible
+print("kkkkk")
+print("k"*5) #result: "kkkkk"
+
+#Int
+print(5)
+print(-10)
+print(3.14)
+print(10000000)
+
+#Automatically calculated
+print(5+3)
+print(2*8)
+
+#Boolean
+print(True)
+print(False)
+print(5 > 10)
+print(not True)
+print(not (5>10)) #result: True
