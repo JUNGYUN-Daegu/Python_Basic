e276426cd239f718a73c139b3b1eaad3bc259ed6
JO JUNGYUN <66199062+JUNGYUN-Daegu@users.noreply.github.com>
2020-10-05 16:23:27 +0900

    Variables

---
+++ Variables.py
@@ -0,0 +1,11 @@
+#Let me introduce my pet
+animal = "Dog"
+name = "Teemo"
+age = 3
+hobby = "Walking"
+is_adult = age >= 3
+
+print("My " + animal + "s Name is " + name)
+#Using Comma, you don't need to type cast other data types into Str
+print(name +" is ", age, "years old, and does like to ", hobby)
+print("is Teemo an adult dog? " + str(is_adult))
